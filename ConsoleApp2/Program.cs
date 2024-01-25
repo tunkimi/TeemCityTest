ccbc69d0df37084d961d5cc357f1c27b799fc4aa
obraztsov <obraztsov@tbss.ru>
2024-01-25 15:44:56 +0300

    mrg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ratata");
+            Console.ReadKey();
             int a = "fdgdfg";
         }
     }
