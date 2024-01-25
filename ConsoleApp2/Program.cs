f71a2e127a37cdca055349a921b8ac7cda26f50b
obraztsov <obraztsov@tbss.ru>
2024-01-25 16:01:12 +0300

    111

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ratata1");
+            Console.WriteLine("ratata55asdasd5");
         }
     }
 }
